3ad54b1c5f999e5d10c13bbb9e9cc7133b0c4276
Stuart Hicks <stuart.hicks@gmail.com>
2024-01-11 18:02:25 +0100

    feat: Adding the Home Link to the mobile menu

--- src/app/components/menus/MenuMobile.tsx
+++ src/app/components/menus/MenuMobile.tsx
@@ -9,33 +9,36 @@ const MenuMobile = () => {
   const dropdownRef = useRef(null)
   
   return (
-    <div ref={dropdownRef} className='md:hidden bg-primaryContrast'>
-      <Hamburger toggled={isOpen} size={20} color="#ffffff" toggle={setIsOpen} />
-      {isOpen && (
-        <div className='fixed left-0 shadow-4xl right-0 p-5 pt-0 bg-primaryDark'>
-          <ul className="p-1">
-            {menudata.map((page, i) => {
-              const { title, link } = page
-              return (
-                <li
-                  key={i}
-                  className='w-full mt-4 rounded-xl bg-primaryContrast'
-                >
-                  <Link
-                    onClick={() => setIsOpen((prev) => !prev)}
-                    className={
-                      "flex items-center justify-between w-full p-5 rounded-xl bg-primaryContrast"
-                    }
-                    href={link}
+    <div className="flex items-center justify-between md:hidden bg-primaryContrast">
+      <Link className="pl-4 text-primaryLight" href="/">Home</Link>
+      <div ref={dropdownRef}>
+        <Hamburger toggled={isOpen} size={20} color="#ffffff" toggle={setIsOpen} />
+        {isOpen && (
+          <div className='fixed left-0 shadow-4xl right-0 p-5 pt-0 bg-primaryDark'>
+            <ul className="p-1">
+              {menudata.map((page, i) => {
+                const { title, link } = page
+                return (
+                  <li
+                    key={i}
+                    className='w-full mt-4 rounded-xl bg-primaryContrast'
                   >
-                    <span className='flex gap-1 text-lg text-primaryLight'>{title}</span>
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-      )}
+                    <Link
+                      onClick={() => setIsOpen((prev) => !prev)}
+                      className={
+                        "flex items-center justify-between w-full p-5 rounded-xl bg-primaryContrast"
+                      }
+                      href={link}
+                    >
+                      <span className='flex gap-1 text-lg text-primaryLight'>{title}</span>
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
