ef1341055c009c899cfc6de71e0143ab80edfa0a
shicks1080 <stuart.hicks@gmail.com>
2024-01-16 09:21:04 +0100

    feat: Adding animation to the menu
    Not sure I like it! But an interesting thing to have tried and leave for future reference.

--- src/app/components/menus/MenuMobile.tsx
+++ src/app/components/menus/MenuMobile.tsx
@@ -1,9 +1,11 @@
 "use client"
-import menudata from "@/app/data/menu"
-import Hamburger from "hamburger-react"
 import Link from "next/link"
 import { useRef, useState } from "react"
 import { useClickAway } from "react-use"
+
+import menudata from "@/app/data/menu"
+import Hamburger from "hamburger-react"
+import { AnimatePresence, motion } from "framer-motion"
 
 const MenuMobile = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -12,35 +14,60 @@ const MenuMobile = () => {
   useClickAway(dropdownRef, () => setIsOpen(false))
 
   return (
-    <div className="flex items-center justify-between md:hidden bg-primaryContrast">
-      <Link className="pl-4 text-primaryLight" href="/">Home</Link>
+    <div className='flex items-center justify-between md:hidden bg-primaryContrast'>
+      <Link className='pl-4 text-primaryLight' href='/'>
+        Home
+      </Link>
       <div ref={dropdownRef}>
-        <Hamburger toggled={isOpen} size={20} color="#ffffff" toggle={setIsOpen} />
-        {isOpen && (
-          <nav className='fixed left-0 shadow-4xl right-0 p-5 pt-0 bg-primaryDark z-10'>
-            <ul className="p-1">
-              {menudata.map((page, i) => {
-                const { title, link } = page
-                return (
-                  <li
-                    key={i}
-                    className='w-full mt-4 rounded-xl bg-primaryContrast'
-                  >
-                    <Link
-                      onClick={() => setIsOpen((prev) => !prev)}
-                      className={
-                        "flex items-center justify-between w-full p-5 rounded-xl bg-primaryContrast"
-                      }
-                      href={link}
+        <Hamburger
+          toggled={isOpen}
+          size={20}
+          color='#ffffff'
+          toggle={setIsOpen}
+        />
+        <AnimatePresence>
+          {isOpen && (
+            <motion.nav 
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.2 }}
+              className='fixed left-0 shadow-4xl right-0 p-5 pt-0 bg-primaryDark z-10'
+            >
+              <ul className='p-1'>
+                {menudata.map((page, i) => {
+                  const { title, link } = page
+                  return (
+                    <motion.li
+                      initial={{ scale: 0, opacity: 0 }}
+                      animate={{ scale: 1, opacity: 1 }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 260,
+                        damping: 20,
+                        delay: 0.1 + i/10,
+                      }}
+                      key={i}
+                      className='w-full mt-4 rounded-xl bg-primaryContrast'
                     >
-                      <span className='flex gap-1 text-lg text-primaryLight'>{title}</span>
-                    </Link>
-                  </li>
-                )
-              })}
-            </ul>
-          </nav>
-        )}
+                      <Link
+                        onClick={() => setIsOpen((prev) => !prev)}
+                        className={
+                          "flex items-center justify-between w-full p-5 rounded-xl bg-primaryContrast"
+                        }
+                        href={link}
+                      >
+                        <span className='flex gap-1 text-lg text-primaryLight'>
+                          {title}
+                        </span>
+                      </Link>
+                    </motion.li>
+                  )
+                })}
+              </ul>
+            </motion.nav>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   )
